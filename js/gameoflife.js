5093df7903f9bf1e2926d3ab349b5f828ee1f3ab
Abdelaziz Alsabagh <dev.alsabagh@gmail.com>
2021-10-16 16:03:32 +0200

    add: getNeighborsOf function

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -53,7 +53,13 @@ const printCells = (state) => {
   return result;
 };
 
-const getNeighborsOf = ([x, y]) => {};
+const getNeighborsOf = ([x, y]) => {
+  return [
+    [x-1,y+1],[x,y+1],[x+1,y+1],
+    [x-1,y],         [x+1,y],
+    [x-1,y-1],[x,y-1],[x+1,y-1]
+  ]
+};
 
 const getLivingNeighbors = (cell, state) => {};
 
